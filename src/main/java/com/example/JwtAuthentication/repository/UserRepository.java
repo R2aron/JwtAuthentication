d0591b9d100ee5fe6700aba95bab8e9e1376e03d
Andrei Aron Racea <raceaaron0@gmail.com>
2025-12-28 10:48:27 +0200

    Initial commit

---
+++ src/main/java/com/example/JwtAuthentication/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.JwtAuthentication.repository;
+
+import com.example.JwtAuthentication.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByVerificationCode(String verificationCode);
+}
